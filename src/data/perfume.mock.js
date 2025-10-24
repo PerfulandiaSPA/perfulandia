98297776d6ecc8279be5e122a1e9d9adc6c08261
R0666 <r.jerez@duocuc.cl>
2025-10-24 14:40:07 -0400

    Agrego mock.js (donde se categforizará de alguna manera los perfumes)

---
+++ src/data/perfume.mock.js
@@ -0,0 +1,60 @@
+// src/data/gaming.mock.js
+export const CATEGORIES = ['']; //categorías de algún tipo para filtrar perfumes 
+
+export const GAMING_PRODUCTS = [
+    {
+        id: 1,
+        name: 
+        price: 
+        category: 
+        imageUrl: 
+    },
+    {
+        id: 2,
+        name: 
+        price: 
+        category: 
+        imageUrl: 
+    },
+    {
+        id: 3,
+        name: 
+        price: 
+        category: 
+        imageUrl: 
+    },
+    {
+        id: 4,
+        name: 
+        price: 
+        category: 
+        imageUrl: 
+    },
+    {
+        id: 5,
+        name: 
+        price: 
+        category: 
+        imageUrl: 
+    },
+    {
+        id: 6,
+        name: 
+        price: 
+        category: 
+        imageUrl: 
+    },
+    {
+        id: 7,
+        name: 
+        price: 
+        category: 
+        imageUrl: 
+    },
+    {
+        id: 8,
+        name: 
+        price: 
+        category: 
+        imageUrl: 
+    }
