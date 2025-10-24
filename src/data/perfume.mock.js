fab7b6f217fddb9466d87fe4fd23acc43399cd93
R0666 <r.jerez@duocuc.cl>
2025-10-24 19:32:50 -0400

    Arreglo enlaces rotos de imágenes de perfumes en el mock

--- src/data/perfume.mock.js
+++ src/data/perfume.mock.js
@@ -8,42 +8,42 @@ export const PERFUMES_LIST = [
         name: "Chanel No. 5",
         price: 120000,  
         category: "Mujer",
-        imageUrl: "https://www.chanel.com/t5/dynimg/perfume/chanel-no-5-eau-de-parfum.jpg"
+        imageUrl: "https://clchanel.vtexassets.com/arquivos/ids/162033/3145891254303_1.jpg?v=638204723225900000"
     },
     {
         id: 2,
         name: "Dior Sauvage",
         price: 110000,  
         category: "Mujer",
-        imageUrl: "https://www.dior.com/couture/var/dior/storage/images/perfumes/dior-sauvage/diorsauvage.jpg"
+        imageUrl: "https://www.dior.com/dw/image/v2/BGXS_PRD/on/demandware.static/-/Sites-master_dior/default/dwad9cc9b4/Y0998004/Y0998004_C099600455_E01_RHC.jpg?sw=1024"
     },
     {
         id: 3,
         name: "Yves Saint Laurent Black Opium",
         price: 95000,  
         category: "Mujer",
-        imageUrl: "https://www.yslbeauty.com/media/wysiwyg/black_opium.jpg"
+        imageUrl: "https://fimgs.net/mdimg/perfume-thumbs/375x500.25324.2x.avif"
     },
     {
         id: 4,
         name: "Gucci Bloom",
         price: 105000,  
         category: "Mujer",
-        imageUrl: "https://www.gucci.com/us/en/pr/women/fragrances-for-women/gucci-bloom-eau-de-parfum-p-410453"
+        imageUrl: "https://fimgs.net/mdimg/perfume-thumbs/375x500.44894.2x.avif"
     },
     {
         id: 5,
         name: "Acqua di Gioia by Giorgio Armani",
         price: 85000,  
         category: "Mujer",
-        imageUrl: "https://www.armani.com/images/large/02-5_1_3_GiorgioArmani_AcquaDiGioia_1.jpg"
+        imageUrl: "https://fimgs.net/mdimg/perfume-thumbs/375x500.8442.2x.avif"
     },
     {
         id: 6,
         name: "Versace Bright Crystal",
         price: 98000,  
         category: "Mujer",
-        imageUrl: "https://www.versace.com/gb/en/versace-bright-crystal-eau-de-toilette-57460982/"
+        imageUrl: "https://fimgs.net/mdimg/perfume-thumbs/375x500.632.2x.avif"
     },
 
     // Categoría Hombre
@@ -53,41 +53,41 @@ export const PERFUMES_LIST = [
         name: "Tom Ford Black Orchid",
         price: 130000,  
         category: "Hombre",
-        imageUrl: "https://www.tomford.com/en-us/black-orchid-eau-de-parfum-100ml/"
+        imageUrl: "https://fimgs.net/mdimg/perfume-thumbs/375x500.62199.2x.avif"
     },
     {
         id: 8,
         name: "Jean Paul Gaultier Le Male",
         price: 88000,  
         category: "Hombre",
-        imageUrl: "https://www.jeanpaulgaultier.com/en-us/le-male/"
+        imageUrl: "https://fimgs.net/mdimg/perfume-thumbs/375x500.430.2x.avif"
     },
     {
         id: 9,
         name: "Dior Homme Intense",
         price: 115000,  
         category: "Hombre",
-        imageUrl: "https://www.dior.com/couture/var/dior/storage/images/perfumes/dior-homme-intense/diordhommeintense.jpg"
+        imageUrl: "https://http2.mlstatic.com/D_NQ_NP_2X_871682-MLA48265216148_112021-F.webp"
     },
     {
         id: 10,
         name: "Hugo Boss Bottled",
         price: 98000,  
         category: "Hombre",
-        imageUrl: "https://www.hugoboss.com/media/wysiwyg/hb/hb_bottled_hero.jpg"
+        imageUrl: "https://fimgs.net/mdimg/perfume-thumbs/375x500.383.2x.avif"
     },
     {
         id: 11,
         name: "Acqua di Parma Colonia",
         price: 160000,  
         category: "Hombre",
-        imageUrl: "https://www.acquadiparma.com/media/wysiwyg/colonia/acqua-di-parma-colonia.jpg"
+        imageUrl: "https://fimgs.net/mdimg/perfume-thumbs/375x500.1681.2x.avif"
     },
     {
         id: 12,
         name: "Paco Rabanne 1 Million",
         price: 105000,  
         category: "Hombre",
-        imageUrl: "https://www.pacorabanne.com/media/wysiwyg/1millionbottle.jpg"
+        imageUrl: "https://fimgs.net/mdimg/perfume-thumbs/375x500.3747.2x.avif"
     }
 ];
