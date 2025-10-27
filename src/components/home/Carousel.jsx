632d248330970be6c54c5c692814551d13e49452
R0666 <r.jerez@duocuc.cl>
2025-10-26 21:24:48 -0400

    carousel funcionando 100%

--- src/components/home/Carousel.jsx
+++ src/components/home/Carousel.jsx
@@ -8,24 +8,19 @@ function IndividualIntervalsExample() {
       <Carousel.Item interval={1000}>
         <ExampleCarouselImage text="First slide" />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3></h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={500}>
         <ExampleCarouselImage text="Second slide" />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3></h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <ExampleCarouselImage text="Third slide" />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <h3></h3>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
--- src/components/home/ExampleCarouselImage.jsx
+++ src/components/home/ExampleCarouselImage.jsx
@@ -1,12 +1,22 @@
 function ExampleCarouselImage({ text }) {
+  // imagen por defecto = primer slide
+  let imgSrc = '/resources/images/carousel_1.png';
+
+  if (text === 'Second slide') {
+    imgSrc = '/resources/images/carousel_2.png';
+  } else if (text === 'Third slide') {
+    imgSrc = '/resources/images/carousel_3.png';
+  }
+
   return (
     <img
       className="d-block w-100"
-      src="/resources/images/carousel.png"  
+      src={imgSrc}
       alt={text}
-      style={{ height: '420px', objectFit: 'cover' }} 
+      style={{ height: '420px', objectFit: 'cover' }}
     />
   );
 }
 
 export default ExampleCarouselImage;
+
