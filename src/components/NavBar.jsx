5e5f07a81ee37bb3d7b380783fadcbe521508f76
DebugggMax <max.olguin@duocuc.cl>
2025-10-27 00:12:35 -0300

    Borre lo inecesario y hay que ordenarlos icono y crea nueva pantalla de carrito

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,9 +26,9 @@ function NavBar() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
+
               <Nav.Link as={Link} to="/login">Login</Nav.Link>
-              <Nav.Link as={Link} to="/perfumes">Perfumes</Nav.Link>
+
 
               {/* Botón del carrito */}
               <div className="cart-btn-container">
