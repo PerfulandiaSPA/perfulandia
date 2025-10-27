6e7c21ebb1feffa14c7309147d3fccaaab2f0325
DebugggMax <max.olguin@duocuc.cl>
2025-10-26 22:47:08 -0300

    Creo CSS NavBarvertical y ya esta desplegable con filtros

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import { Container } from 'react-bootstrap'; 
-import IndividualIntervalsExample from '../components/home/Carousel'; 
+import { Container } from 'react-bootstrap';
+import IndividualIntervalsExample from '../components/home/Carousel';
 
 function Home() {
   return (
